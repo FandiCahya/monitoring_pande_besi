6f79117327df609a2b6c3c666e4637eb5069b641
FandiCahya <fandicahya56@gmail.com>
2024-08-28 07:51:45 +0700

    New Upload hanya splash page saja

---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+      primaryColor: const Color.fromRGBO(44, 28, 107, 1),
+      scaffoldBackgroundColor: AppColors.lighBackground,
+      brightness: Brightness.light,
+      fontFamily: 'Satoshi',
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(44, 28, 107, 1),
+              textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)))));
+
+  static final darkTheme = ThemeData(
+      primaryColor: const Color.fromRGBO(44, 28, 107, 1),
+      scaffoldBackgroundColor: AppColors.darkBackground,
+      brightness: Brightness.dark,
+      fontFamily: 'Satoshi',
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(44, 28, 107, 1),
+              textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)))));
+}
